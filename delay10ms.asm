d61853987332231bed477cf485d9a94f9136651c
selassie-eye <ollivierre.khari@gmail.com>
2018-02-08 01:25:40 -0500

    New files

---
+++ delay10ms.asm
@@ -0,0 +1,47 @@
+/****
+ * lab2b.asm
+ *
+ * Khari Ollivierre
+****/
+
+.include "ATxmega128A1Udef.inc"
+
+.equ LIMIT = 0xFF
+.equ DELAY = 0x19
+.equ ZERO = 0x00
+
+.ORG 0x0000					
+	rjmp DELAY10MS				
+
+.ORG 0x0100
+
+DELAY10MS:
+	ldi R11, ZERO	; Zero registers for counting
+	ldi R12, ZERO
+	ldi R13, LIMIT
+	sts PORTD_DIRSET, R13 ; Allow Port D to output the delayed clock
+	sts PORTD_OUTCLR, R13
+
+DLOOP:
+	inc R11		; Increment counter register
+	cpi R11, LIMIT	; Counter check
+	brne DLOOP	; This loops 256 times, 3*256 = 768 instructions per loop
+			; It takes the CPU 10 ms to execute 20000 instructions,
+			; and by the start of the loop, it has executed 2 instructions, so
+			; 19998 instructions are left to be executed.
+			;
+			; 19998/3 = 6666, so the loop would have to execute 6666 times to delay appropriately.
+			; Since the registers only hold 2 bytes each, multiple registers must track the loop 
+			; execution.
+	inc R12		; increments the 2nd counter
+	cpi R12, DELAY	; counter check
+	breq END	; Each increment will execute 771 instructions. 19998/771 ~= 26, so this entire loop
+			; should execute 26 times. The end result will be marginally identical to 10 ms
+	ldi R11, ZERO
+	rjmp DLOOP
+END:
+	sts PORTD_OUTTGL, R13	; Toggle the output like a clock
+	ldi R11, ZERO
+	rjmp DLOOP
+	
+
---
+++ lab2b.asm
@@ -0,0 +1,25 @@
+/****
+ * lab2b.asm
+ *
+ * Khari Ollivierre
+****/
+
+.include "ATxmega128A1Udef.inc"
+
+.equ ONES = 0xFF
+.equ ZERO = ~ONES
+
+.ORG 0x0000					
+	rjmp MAIN				
+
+.ORG 0x0100
+
+MAIN:
+	ldi R16, ONES
+	sts PORTA_DIRCLR, R16	; Configures Port A to receive switch input
+	sts PORTC_DIRSET, R16	; Configures Port C to output to LEDs
+	sts PORTC_OUTSET, R16	; Turns off LEDs (active-low)
+	sts PORTA_IN, PORTC_OUT ; Sets the output to the switch state
+	rjmp MAIN		; Loops forever
+	
+	
